adc5f42b48e668d134015dfb2cda9451ea233ffe
Nur Shuvo <nurshuvo51@gmail.com>
2017-03-20 00:32:53 +0600

    Add files via upload

---
+++ uva 10341.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+double p,q,r,s,t,u;
+
+// it is sufficient to check the signs for f(0) and f(1) to determine whether there is a root in range [0,1].
+
+double res(double x)
+{
+   return p*exp(-x) + q*sin(x) + r*cos(x) + s*tan(x) + t*x*x + u;
+}
+
+int main()
+{
+    while(scanf("%lf %lf %lf %lf %lf %lf",&p,&q,&r,&s,&t,&u)==6){
+    double lo = 0.0,hi = 1.0,mid;
+    int ck = 0;
+    for(int i=0;i<1000;i++){
+          mid = (lo+hi)/2.0;
+          if(res(mid)<=0.0) hi = mid;
+          else lo = mid;
+    }
+
+//    if (res(0) * res(1) > 0)
+//      puts("No solution");
+    if(res(mid)>=0.000001) puts("No solution");
+
+    else printf("%0.4lf\n",mid);
+    printf("%lf\n",mid);
+    }
+}
